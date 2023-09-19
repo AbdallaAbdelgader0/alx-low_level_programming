ed03add2d4aaa5ead621887114c97f51cf0cb1a2
AbdallaAbdelgader0 <abdoalzeer3@gmail.com>
2023-09-18 17:11:27 -0700

    fdew

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sterlin - return the length of the string
+ * 
+ * @s: string parameter input
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '/0'; s++)
+	++counter;
+
+	return counter;
+}
